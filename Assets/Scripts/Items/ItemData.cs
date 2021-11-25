16bcc1f03d304fcb15c7337619058604168e0a09
Austin <austinrdelgado@gmail.com>
2021-11-24 19:28:39 -0500

    Set up for new item system

---
+++ Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemData
+{
+    public int ID;
+    public string Name;
+    public string Description;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
